591ddb8093947f8076118f1e869070f7c26126a8
mauditecandela <candela.jimenez@gmail.com>
2018-07-22 21:11:36 +0200

    Moves the initial state and componentWillMount to Body and passes the state through props to AllBooks

--- app/assets/javascripts/components/_all_books.js.jsx
+++ app/assets/javascripts/components/_all_books.js.jsx
@@ -1,22 +1,6 @@
 class AllBooks extends React.Component {
-  constructor(){
-      super()
-      this.state = {
-        books: []
-      }
-    }
-
-  componentDidMount() {
-    fetch('/api/v1/books.json')
-			.then(response => response.json())
-			.then(data => {
-				this.setState({books: data })
-		})
-			.catch(err => console.error(this.props.url, err.toString()))
-	}
-
   render() {
-    var books = this.state.books.map((book) => {
+    var books = this.props.books.map((book) => {
       return (
         <div key={book.id}>
           <h3>{book.name}</h3>
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,8 +1,24 @@
 class Body extends React.Component {
+  constructor(){
+      super()
+      this.state = {
+        books: []
+      }
+    }
+
+  componentDidMount() {
+    fetch('/api/v1/books.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({books: data })
+    })
+      .catch(err => console.error(this.props.url, err.toString()))
+  }
+
   render(){
     return (
       <div>
-        <AllBooks />
+        <AllBooks books={this.state.books}/>
         <NewBook />
       </div>
     )
